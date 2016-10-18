d9395b7db941810b1b18bb0936bdb39f215446b6
Ineke Scheffers <ineke.scheffers@hotmail.com>
2016-10-18 17:51:15 +0200

    made request.body readable by installing and using bodyparser, log the input of the user in the search form

--- 10-24-2016-User-Information-App-Web-Server/app.js
+++ 10-24-2016-User-Information-App-Web-Server/app.js
@@ -1,6 +1,9 @@
 const express = require('express') 	// require express library
 const fs = require('fs')			// require fs library
+const bodyParser = require('body-parser')
 const app = express()				// create app as instance of express
+//app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}))
 
 app.set('view engine', 'pug') // set view engine to pug
 app.set('views', __dirname + '/views') //set where the view engine is located
@@ -36,6 +39,19 @@ app.get('/add-user', (request, response) => {
 		response.render('add-user', {data: parsedData})
 	})
 }) 
+
+app.post('/result-search', (request, response) => {
+	console.log(request.body);
+})
+
+// app.post(/*--data "firstname=value&lastname=value&email=value", */'/all-users', (request, response) => {
+// 	console.log("About to render the all-users.pug page with the new added user...");
+// 	fs.readFile(__dirname + '/users.json', (err, data) => {
+// 		if (err) throw err;
+// 		let parsedData = JSON.parse(data);
+// 		response.render('all-users', {data: parsedData})
+// 	})
+// })
 
 app.listen(8000, () =>{
 	console.log('server is running');
