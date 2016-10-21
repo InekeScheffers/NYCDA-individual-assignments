e91e01e32701da5ab6338b3f7fad077216f82e41
Ineke Scheffers <ineke.scheffers@hotmail.com>
2016-10-21 15:00:37 +0200

    also deleted alert message for search user, to the friendlier red error message above the search bar

--- 10-24-2016-User-Information-App-Web-Server/app.js
+++ 10-24-2016-User-Information-App-Web-Server/app.js
@@ -62,7 +62,7 @@ app.post('/result-search', (request, response) => {
 
 		// if user wasn't found because search field was empty, user doesn't exist or user is misspelled stay on page and give alert message, else go to results and show result
 		if(result.length === 0) { // if result has no objects in it (because no match or no search query)
-			response.render('search-user', {data: result}); // stay on search-user, pass data so search-user.pug can give an alert to try again or add user
+			response.render('search-user', {fieldEmptyError: true, errorMessage: "Oops, didn't find user. Please, search again (remember to start with a capital letter)."}); // stay on search-user, pass error to search-user so it can give the passed error message to try again
 		} else {
 			console.log("About to render the result-search.pug page..."); // if there is one or more objects in result
 			response.render('result-search', {data: result, searchQuery: request.body.search}) // render /result-search, and pass data so /result-search.pug can show the found users
@@ -72,7 +72,7 @@ app.post('/result-search', (request, response) => {
 
 app.post('/all-users', (request, response) => {
 	if(!request.body['firstname'] || !request.body['lastname'] || !request.body['email']){ // if one of the fields is empty
-		response.render('add-user', {fieldEmptyError: true, errorMessage: 'Try again. Please fill out all fields to add a user.'}) // stay on add-user, pass error to add-user so it can give an alert to fill out all fields to add a user
+		response.render('add-user', {fieldEmptyError: true, errorMessage: 'Try again. Please fill out all fields to add a user.'}) // stay on add-user, pass error to add-user so it can give the passed error message to fill out all fields to add a user
 	} else {
 		fs.readFile(__dirname + '/users.json', (err, data) => { // if all fields are filled out
 			if (err) throw err;
