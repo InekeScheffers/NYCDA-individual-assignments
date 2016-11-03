2ac1713dfcfc0ec3bb9cf826be154e4f2eca3aad
Ineke Scheffers <ineke.scheffers@hotmail.com>
2016-11-03 16:29:10 +0100

    cleaned up pug syntax so it's more consistent overall

--- 11-08-2016-Bulletin-Board-Application/static/js/main.js
+++ 11-08-2016-Bulletin-Board-Application/static/js/main.js
@@ -1,6 +1,8 @@
-$( () => {
-	//highlight current nav
-	// looks for # on body, and if a (link) contains the string, than adds a class to that link: active
-	$("#leavemessage a:contains('Leave a message')").parent().addClass('active');
-	$("#bulletinboard a:contains('Bulletin board')").parent().addClass('active');
-})
+$(document).ready( () => {
+	$( () => {
+		//highlight current nav
+		// looks for # on body, and if a (link) contains the string, than adds a class to that link: active
+		$("#leavemessage a:contains('Leave a message')").parent().addClass('active');
+		$("#bulletinboard a:contains('Bulletin board')").parent().addClass('active');
+	})
+});
