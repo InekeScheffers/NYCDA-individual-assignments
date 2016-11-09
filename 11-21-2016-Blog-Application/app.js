41746b88d38750fedf4c89538234328a4060dc9e
Ineke Scheffers <ineke.scheffers@hotmail.com>
2016-11-09 16:18:37 +0100

    register now create a new user row in table users

--- 11-21-2016-Blog-Application/app.js
+++ 11-21-2016-Blog-Application/app.js
@@ -8,24 +8,61 @@ const session = require('express-session')
 const app = express()
 
 // connect to database blogapp
-const sequelize = new Sequelize('blogapp', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+let db = new Sequelize('blogapp', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
 	host: 'localhost',
 	dialect: 'postgres'
 })
 
 // Parse incoming request bodies in a middleware before your handlers, availabe under the req.body property
 app.use(bodyParser.urlencoded({ extended: true }))
-
 // serve static files in express
-app.use(express.static(__dirname + '/static/'))
+app.use(express.static(__dirname + '/static'))
+// activate/configure session
+app.use(session({
+	// passphrase
+	secret: 'oh wow very secret much security',
+	resave: true,
+	saveUninitialized: false
+}));
 
 // set view engine to pug & set where the view engine is located
 app.set('view engine', 'pug')
 app.set('views', __dirname + '/views')
 
-app.get('/ping', (request, response) => {
-	response.send('pong')
+// test if app works
+// app.get('/ping', (request, response) => {
+// 	response.send('pong')
+// })
+
+// when home is requested render localhost:8000
+app.get('/', (request, response) => {
+	console.log("About to render the register/login page...");
+	response.render('login')
 })
+
+// When submit button is clicked on leave a login.pug
+app.post('/', (request, response) => {	
+    // create new user (row) in table users
+	User.create ({
+		name: 		request.body.name, 
+		email: 		request.body.email, 
+		password: 	request.body.password
+	})
+	.then( () => {
+		response.render('newsfeed');
+	})
+})
+
+// create model for tables users
+let User = db.define('user', {
+	// say name and email have to be unique in this table
+	name: {type: Sequelize.STRING, unique: true},
+	email: {type: Sequelize.STRING, unique: true},
+	password: Sequelize.STRING
+});
+
+// {force: true}: so this table is deleted from database
+User.sync({force:true})
 
 // set up port to locally run your app in the browser
 app.listen(8000, () =>{
